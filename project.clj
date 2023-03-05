1e468482072c5c87481a254b21005d9d6f476cab
Alfian Pratama <alfianpratama@hotmail.com>
2023-03-05 21:08:09 +0700

    test

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns simpleweb.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.8.2"]
+                 [metosin/ring-http-response "0.9.1"]
+                 [metosin/muuntaja "0.6.7"]
+                 [metosin/reitit "0.5.11"]]
+  :repl-options {:init-ns simpleweb.core}
+  :main simpleweb.core)
--- src/simpleweb/core.clj
+++ src/simpleweb/core.clj
@@ -1,6 +1,45 @@
-(ns simpleweb.core)
+(ns simpleweb.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :as wrap-reload]
+            [ring.util.http-response :as response]
+            [muuntaja.middleware :as muuntaja]
+            [reitit.ring :as reitit]
+            ))
+;; Handler
+(defn html-handler [request-map] 
+  (response/ok
+   (str "<html><body> your IP is: " 
+        (:remote-addr request-map) 
+        "</body></html>")))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn json-handler [request]
+  (response/ok
+   {:result (get-in request [:body-params :id])}))
+
+;; Middleware
+(defn wrap-nocache [handler]
+  (fn [request]
+  (-> request
+      handler
+      (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
+;; Route
+(def routes
+  [["/" {:get html-handler
+         :post html-handler}]])
+
+(def handler
+  (reitit/ring-handler(reitit/router routes)))
+
+(defn -main []
+  (jetty/run-jetty 
+   (-> #'handler 
+       wrap-nocache
+       wrap-formats
+       wrap-reload/wrap-reload)
+   {:port 3000 :join? false}))
+
